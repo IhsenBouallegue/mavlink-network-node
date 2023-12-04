92e941c14cd3f2a986ad4d31e32aabc1d659e0f3
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2023-12-04 21:25:49 +0100

    Add NodeType enum and from_str method

--- src/utils/types.rs
+++ src/utils/types.rs
@@ -11,3 +11,19 @@ pub type LoRaDevice = LoRa<Spi, OutputPin, OutputPin, Delay>;
 pub type MavDevice = Box<dyn MavConnection<MavMessage> + Send + Sync>;
 
 pub type MavFramePacket = MavFrame<MavMessage>;
+
+#[derive(Debug)]
+pub enum NodeType {
+    Drone,
+    Gateway,
+}
+
+impl NodeType {
+    pub fn from_str(s: &str) -> Result<NodeType, ()> {
+        match s {
+            "Drone" => Ok(NodeType::Drone),
+            "Gateway" => Ok(NodeType::Gateway),
+            _ => Err(println!("Invalid node type")),
+        }
+    }
+}
