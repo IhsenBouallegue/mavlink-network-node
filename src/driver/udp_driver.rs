1f8d1a9b417e4d40d8da77c7a753b2a04b2190af
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2023-12-04 21:25:33 +0100

    Refactor UDPDriver create_instance method

--- src/driver/udp_driver.rs
+++ src/driver/udp_driver.rs
@@ -1,6 +1,8 @@
 use super::abstract_driver::Driver;
-use crate::utils::mavlink_utils::{create_groundstation_mavlink, mavlink_receive_blcoking, mavlink_send};
-use crate::utils::types::{MavDevice, MavFramePacket};
+use crate::utils::mavlink_utils::{
+    create_groundstation_mavlink, create_mavlink, mavlink_receive_blcoking, mavlink_send,
+};
+use crate::utils::types::{MavDevice, MavFramePacket, NodeType};
 use std::sync::{Arc, Mutex, RwLock};
 
 pub struct UDPDriver {
@@ -26,9 +28,17 @@ impl Driver<MavFramePacket> for UDPDriver {
     }
 
     fn create_instance() -> Self {
-        // let mavlink = create_groundstation_mavlink();
-        // let mavlink = create_mavlink();
-        let mavlink = create_groundstation_mavlink();
+        let node_type = NodeType::from_str(std::env::var("NODE_TYPE").unwrap().as_str()).unwrap();
+        let mavlink;
+        match node_type {
+            NodeType::Drone => {
+                mavlink = create_mavlink();
+            }
+            NodeType::Gateway => {
+                mavlink = create_groundstation_mavlink();
+            }
+        }
+
         Self {
             driver_instance: Arc::new(RwLock::new(mavlink)),
         }
