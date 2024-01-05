f77bad2c57f55fbe22f7dc982ed3c848a5bd43d4
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2024-01-05 21:28:02 +0100

    Refactor copy_logs.sh script to support multiple target hosts

--- copy_logs.sh
+++ copy_logs.sh
@@ -1,28 +1,41 @@
 #!/bin/bash
 
-readonly TARGET_HOST=$1
 readonly TARGET_USER=node
 readonly TARGET_PATH=/home/${TARGET_USER}/logs
-readonly TARGET_ADDRESS=${TARGET_USER}@${TARGET_HOST}.local
-
-readonly DEST_PATH=./logs/${TARGET_HOST}
-
+readonly DEST_BASE_PATH=./logs
 
 copy_logs() {
-    echo "Copying logs from Raspberry Pi to " ${DEST_PATH} "..."
-    rsync -avz --progress ${TARGET_ADDRESS}:${TARGET_PATH} ${DEST_PATH}
+    local target_host=$1
+    local target_address=${TARGET_USER}@${target_host}.local
+    local dest_path=${DEST_BASE_PATH}/${target_host}
+
+    echo "------COPY-------"
+    echo "Copying logs from Raspberry Pi at ${target_host} to ${dest_path}..."
+    rsync -avz --progress ${target_address}:${TARGET_PATH} ${dest_path}
     echo "Copy complete."
 }
 
 delete_logs() {
-    echo "Deleting logs directory on Raspberry Pi..."
-    ssh ${TARGET_ADDRESS} "rm -rf ${TARGET_PATH}"
+    local target_host=$1
+    local target_address=${TARGET_USER}@${target_host}.local
+
+    echo "------DELETE-------"
+    echo "Deleting logs directory on Raspberry Pi at ${target_host}..."
+    ssh ${target_address} "rm -rf ${TARGET_PATH}"
     echo "Logs directory deleted."
 }
 
-if [ "$2" == "delete" ]; then
-    copy_logs
-    delete_logs
-else
-    copy_logs
-fi
+# Loop through each passed hostname
+for target_host in "$@"
+do
+    if [ "$target_host" == "delete" ]; then
+        # Skip if the argument is 'delete'
+        continue
+    fi
+
+    # Perform operations
+    copy_logs $target_host
+    if [[ "$@" =~ " delete" ]]; then
+        delete_logs $target_host
+    fi
+done
