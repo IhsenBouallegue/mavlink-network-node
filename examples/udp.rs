45c7b86a3ca27361aff1ce5ec3376469785b77ae
Ihsen Bouallegue <bouallegueihsen@gmail.com>
2024-02-25 14:31:56 +0100

    Add full example

--- examples/udp.rs
+++ examples/udp.rs
@@ -1,10 +1,11 @@
 use std::sync::Arc;
 
+use futures::future::join_all;
 use mavlink_network_node::full_duplex_network::FullDuplexNetwork;
 use mavlink_network_node::logging_utils::init_logging;
 use mavlink_network_node::types::NodeType;
 use mavlink_network_node::udp_driver::{UDPConfig, UDPDriver};
-use mavlink_network_node::{NetworkInterface, RunHandle};
+use mavlink_network_node::NetworkInterface;
 
 #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
 async fn main() {
@@ -36,9 +37,7 @@ async fn gateway() {
     let channel_size = 100;
     let (udp_network, _tx, _rx) = FullDuplexNetwork::new(driver, channel_size);
 
-    let run_handle = udp_network.run().await;
+    let run_handles = udp_network.run().await;
 
-    if let RunHandle::Dual(recv_handle, send_handle) = run_handle {
-        let _ = tokio::join!(recv_handle, send_handle);
-    }
+    join_all(run_handles).await;
 }
